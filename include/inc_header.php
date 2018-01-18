af51b81817c2fb9bdab813b5d386058f1f2b2cc1
remichapuis <remichapuis@192.168.0.19>
2018-01-18 22:18:13 +0100

    Modif Header (ajout Jquery par http et api Google )

--- include/inc_header.php
+++ include/inc_header.php
@@ -21,4 +21,7 @@ $titrePage = $page->titrePage;
 <title><?php echo $titrePage ?></title>
 <link rel="stylesheet" type="text/css" href="css/style.css" />
 
+<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+
 <?php include 'include/inc_menu.php';?>
